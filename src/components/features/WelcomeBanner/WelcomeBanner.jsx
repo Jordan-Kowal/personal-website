328921b90cb9518f4458969e431a19c0cf978f08
Jordan Kowal <kowaljordan@gmail.com>
2023-05-19 00:04:39 +0200

    Added video background

--- src/components/features/WelcomeBanner/WelcomeBanner.jsx
+++ src/components/features/WelcomeBanner/WelcomeBanner.jsx
@@ -1,15 +1,15 @@
 import React, { memo } from 'react';
-import { Typography } from 'antd';
 import Socials from '@/components/features/Socials';
 import Logo from '@/components/ui/Logo';
 import Space from '@/components/ui/Space';
 import PresentationText from './PresentationText';
 import styles from './WelcomeBanner.module.less';
 
-export const { Text } = Typography;
-
 const WelcomeBanner = () => (
   <Space className={styles.welcomeBanner} block vertical size={20}>
+    <video autoPlay muted loop className={styles.video}>
+      <source src="videos/background-presentation.mp4" type="video/mp4" />
+    </video>
     <Logo height={80} />
     <PresentationText />
     <Socials />
