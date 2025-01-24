fe9ddca4e02ad3b496a236252e263eb37fad12cd
Jordan Kowal <kowaljordan@gmail.com>
2025-01-24 23:57:24 +0100

    chore: improved conditional react scan

--- vite.config.ts
+++ vite.config.ts
@@ -3,43 +3,44 @@ import million from "million/compiler";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command, mode }) => ({
-  plugins: [
-    million.vite({ auto: true }),
-    react(),
-    {
-      name: "inject-react-scan",
-      transformIndexHtml(html) {
-        if (mode === "development") {
-          return html.replace(
-            "</head>",
-            `<script src="https://unpkg.com/react-scan/dist/auto.global.js"></script></head>`,
-          );
-        }
-        return html;
+const reactScanPlugin = {
+  name: "inject-react-scan",
+  transformIndexHtml(html: string) {
+    return html.replace(
+      "</head>",
+      `<script src="https://unpkg.com/react-scan/dist/auto.global.js"></script></head>`,
+    );
+  },
+};
+
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const plugins = [million.vite({ auto: true }), react()];
+  if (isDev) plugins.push(reactScanPlugin);
+
+  return {
+    plugins: plugins,
+    server: {
+      port: 3000,
+      cors: true,
+    },
+    esbuild: {
+      loader: "tsx",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+          ".ts": "tsx",
+        },
       },
     },
-  ],
-  server: {
-    port: 3000,
-    cors: true,
-  },
-  esbuild: {
-    loader: "tsx",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-        ".ts": "tsx",
-      },
+    resolve: {
+      alias: { "@": resolve(__dirname, "./src") },
     },
-  },
-  resolve: {
-    alias: { "@": resolve(__dirname, "./src") },
-  },
-  build: {
-    assetsDir: "static",
-    chunkSizeWarningLimit: 1024,
-  },
-}));
+    build: {
+      assetsDir: "static",
+      chunkSizeWarningLimit: 1024,
+    },
+  };
+});
