3496ca186871527fbd2c9a1aedd90ac7ded294db
Jordan Kowal <kowaljordan@gmail.com>
2025-01-24 23:51:19 +0100

    fix: react scan in dev mode only

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,23 @@ import million from "million/compiler";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
+export default defineConfig(({ command, mode }) => ({
+  plugins: [
+    million.vite({ auto: true }),
+    react(),
+    {
+      name: "inject-react-scan",
+      transformIndexHtml(html) {
+        if (mode === "development") {
+          return html.replace(
+            "</head>",
+            `<script src="https://unpkg.com/react-scan/dist/auto.global.js"></script></head>`,
+          );
+        }
+        return html;
+      },
+    },
+  ],
   server: {
     port: 3000,
     cors: true,
@@ -27,4 +42,4 @@ export default defineConfig({
     assetsDir: "static",
     chunkSizeWarningLimit: 1024,
   },
-});
+}));
